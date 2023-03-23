520078d77a32fb4632d605d9acf18566bb0276e2
Semas2001 <semasarmonaitis@gmail.com>
2023-03-23 00:53:43 +0000

    made the boxes for different items

---
+++ cart.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+$mysqli = require __DIR__ . "/db.php";
+
+$sql="SELECT id, item, price FROM items";
+$result = $mysqli->query($sql);
+
+if ($result) {
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $_SESSION["id"] = $row["id"];
+        $_SESSION["item"] = $row["item"];
+        $_SESSION["price"] = $row["price"];
+    } else {
+        echo "No results found.";
+    }
+} else {
+    echo "Error executing query: " . $mysqli->error;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Quiz Page</title>
+    <link rel="stylesheet" href="styles.css" />
+        <script
+			  src="https://code.jquery.com/jquery-3.6.3.js"
+			  integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+			  crossorigin="anonymous"></script>
+    
+    <script type = "text/javascript"></script>
+</head>
+<body>
+    <div class='container' >
+        <div class='box'>
+            <?php include 'item_box.php'?>
+            <div class='button'><a href='cart.php'>View Cart</div></a>
+        </div> 
+
+    </div>
+
+
+</body>
--- item_box.php
+++ item_box.php
@@ -1,30 +1,42 @@
 <?php
+
+    function add_to_cart($id){
+        array_push($array, $id);
+    }
     
     function display_item(){
         $mysqli = require __DIR__ . "\db.php";
         $sql = " SELECT * FROM items";
+        $res =$mysqli->query($sql);
+
+        $num = 1;
+
+    
+
+        $sql = " SELECT * FROM items WHERE `id` = $num";
         $result =$mysqli->query($sql);
-
-        $items = $result->fetch_assoc();
-
-        $num = 0;
-
-       /*  $id = $items["id"]; */
-        $item = $items["item"];
-        $price = $items["price"];
-        $row = $result->mysql_fetch_row();
+       
         
 
-        while($row = $result->fetch_assoc()){
-            /* $title = "ITEM: ".$row['id'];
-            $id =  $row['id'];
-            echo "<div id='$id' class='item-box'>";
-            echo" <h1 id='Q$id'>$id: $item,  $price</h1>";
-            echo "</div>"; */
-            ECHO $rew[$num];
+        while($row = $res->fetch_assoc()){
+            $sql = " SELECT * FROM items WHERE `id` = $num";
+            $result =$mysqli->query($sql);
+            $id = $row['id'];
+            $item = $row["item"];
+            $price = $row["price"];
+            echo "<form action=post";
+            echo "<div class='item_box'> ";
+            echo "<div class='item'>$item </div>";
+            echo "<br>";
+            echo "<div class='price'>$price </div>";
+            echo "<input class='button' type=submit></input>";
+            echo "<br><br>";
+            echo "</form>";
+            echo "<br>";
             $num= $num+1;
         }
 
-
     }
+    
+
 ?>
